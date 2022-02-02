e79394fdb55b394efbeef61c9be3967aae7b4f48
Abdurrahman Abolaji <abolajiabdurrahman@gmail.com>
2022-02-02 12:50:56 +0100

    implement starting conditions

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 'use strict';
 
+// SELECTORS
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
 
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+//diceEl.style.display = "none";
+diceEl.classList.add('hidden')
