b3e0a12c03a2a4f065c751710861d845013ae5fa
Abdurrahman Abolaji <abolajiabdurrahman@gmail.com>
2022-02-08 23:39:09 +0100

    hold button re-implemented

--- script.js
+++ script.js
@@ -24,57 +24,64 @@ diceEl.classList.add('hidden');
 let scoresArray = [0,0]
 let currentScore = 0;
 let currentPlayer = 0;
+let inPlay = true;
 
 
 // Rolling dice functionality
 
 rollBtn.addEventListener('click', function () {
-    // 1. Generate random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    //console.log(dice);
+    if (inPlay) {
+        // 1. Generate random dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
+        //console.log(dice);
 
-    // 2. Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
-    
+        // 2. Display dice
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
+        
 
-    // 3. Check for rolled 1.
+        // 3. Check for rolled 1.
 
-    if (dice !== 1) {
-        // Add dice to current score
-        currentScore += dice;
-        document.getElementById(`current--${currentPlayer}`).textContent = currentScore;
-        //current0El.textContent = currentScore;  // CHANGE LATER
-    } else {
-        // Switch to the next player
-        currentScore = 0;
-        document.getElementById(`current--${currentPlayer}`).textContent = 0;
-        currentPlayer = currentPlayer === 0 ? 1 : 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
-        //current0El.textContent = 0;
+        if (dice !== 1) {
+            // Add dice to current score
+            currentScore += dice;
+            document.getElementById(`current--${currentPlayer}`).textContent = currentScore;
+            //current0El.textContent = currentScore;  // CHANGE LATER
+        } else {
+            // Switch to the next player
+            currentScore = 0;
+            document.getElementById(`current--${currentPlayer}`).textContent = 0;
+            currentPlayer = currentPlayer === 0 ? 1 : 0;
+            player0El.classList.toggle('player--active');
+            player1El.classList.toggle('player--active');
+            //current0El.textContent = 0;
+        }
     }
 })
 
 
 holdBtn.addEventListener('click', function() {
-    // 1. Add current score to current player's score
-    scoresArray[currentPlayer] += currentScore;
-    //console.log(currentScore);
-    document.getElementById(`score--${currentPlayer}`).textContent = scoresArray[currentPlayer]; 
-    console.log(currentScore);
+    if (inPlay) {
+       // 1. Add current score to current player's score
+        scoresArray[currentPlayer] += currentScore;
+        //console.log(currentScore);
+        document.getElementById(`score--${currentPlayer}`).textContent = scoresArray[currentPlayer]; 
+        console.log(currentScore);
 
-    // 2. Check if score is >= 30
-    if (scoresArray[currentPlayer] >= 100) {
-        document.querySelector(`.player--${currentPlayer}`).classList.add('player--winner');
-        document.querySelector(`.player--${currentPlayer}`).classList.remove('player--active');
-
-    } else{
-        currentScore = 0;
-        document.getElementById(`current--${currentPlayer}`).textContent = 0;
-        currentPlayer = currentPlayer === 0 ? 1 : 0
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+        // 2. Check if score is >= 30
+        if (scoresArray[currentPlayer] >= 20) {
+            document.querySelector(`.player--${currentPlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${currentPlayer}`).classList.remove('player--active');
+            inPlay = false;
+            diceEl.classList.add('hidden');
+        } else{
+            currentScore = 0;
+            document.getElementById(`current--${currentPlayer}`).textContent = 0;
+            currentPlayer = currentPlayer === 0 ? 1 : 0
+            player0El.classList.toggle('player--active');
+            player1El.classList.toggle('player--active');
+        } 
     }
+    
     
 })
